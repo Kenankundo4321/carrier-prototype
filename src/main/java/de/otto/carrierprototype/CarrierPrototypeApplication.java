5919aed8b6d68349f103f77051aff3d1f2d6675d
KenanKundo4321 <kenan.kundo@otto.de>
2020-09-16 15:37:27 +0200

    [LF-1023] initial commit

---
+++ src/main/java/de/otto/carrierprototype/CarrierPrototypeApplication.java
@@ -0,0 +1,13 @@
+package de.otto.carrierprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierPrototypeApplication.class, args);
+	}
+
+}
